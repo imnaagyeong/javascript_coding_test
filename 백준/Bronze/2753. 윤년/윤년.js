a47850f4473a574c8c9b3dfd65a921afec518785
Na-gang99 <155069538+Na-gang99@users.noreply.github.com>
2024-02-08 14:08:38 +0900

    [Bronze V] Title: 윤년, Time: 136 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+const year = parseInt(input);
+
+if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+    console.log("1");
+}
+else{
+    console.log("0");
+}
