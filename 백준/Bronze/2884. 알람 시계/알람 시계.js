38e001ce14af26108d928245372c9f35387200a1
Na-gang99 <155069538+Na-gang99@users.noreply.github.com>
2024-02-08 14:36:09 +0900

    [Bronze III] Title: 알람 시계, Time: 132 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -1,25 +1,14 @@
-const readline = require("readline");
-const rl = readline.createInterface(
-    {
-        input: process.stdin,
-        output: process.stdout,
-    }
-);
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().split(" ");
+const H = parseInt(input[0]);
+const M = parseInt(input[1]);
 
-let input = [];
-rl.on('line', function(line){
-    input = line.split(' ').map(el => parseInt(el));
-}).on('close', function(close){
-    const H = input[0];
-    const M = input[1];
-
-    if(M >= 45){
-        console.log(H + " " + (M-45));
-    }
-    else if(H === 0){
-        console.log("23 " + (M + 15))
-    }
-    else{
-        console.log((H-1) + " " + (M + 15))
-    }
-});
+if(M >= 45){
+    console.log(H + " " + (M-45));
+}
+else if(H === 0){
+    console.log("23 " + (M + 15))
+}
+else{
+    console.log((H-1) + " " + (M + 15))
+}
