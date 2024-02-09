5219cd09b4e8502a17250983c6ea90ff154b3874
Na-gang99 <155069538+Na-gang99@users.noreply.github.com>
2024-02-09 10:41:27 +0900

    [Bronze V] Title: A+B - 7, Time: 120 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().split("\n");
+let str = "";
+for (let i = 0; i < Number(input[0]); i++) {
+    const nums = input[i + 1].split(" ");
+    str += "Case #"+ (i+1) + ": " + (Number(nums[0]) + Number(nums[1])) + "\n";
+}
+console.log(str);
