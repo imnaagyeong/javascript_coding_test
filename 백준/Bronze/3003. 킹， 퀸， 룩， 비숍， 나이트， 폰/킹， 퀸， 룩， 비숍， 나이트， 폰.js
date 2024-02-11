19b41ba6e0505215801f746ef3c7354c510df8a0
Na-gang99 <155069538+Na-gang99@users.noreply.github.com>
2024-02-11 20:47:44 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.js
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.js
@@ -1,10 +1,6 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().split(" ");
 const counts = [1,1,2,2,2,8];
-const needs = [];
-
-for(let i = 0; i < counts.length ; i++){
-    needs[i] = counts[i] - input[i]
-}
+const needs = counts.map((ele, idx)=> ele - input[idx]);
 
 console.log(needs.join(" "));
