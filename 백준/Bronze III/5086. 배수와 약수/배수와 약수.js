ffaa130128abbb7af11a3a006adde073fdf87fc8
Na-gang99 <155069538+Na-gang99@users.noreply.github.com>
2024-02-16 09:20:27 +0900

    [Bronze III] Title: 배수와 약수, Time: 120 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze III/5086. 배수와 약수/배수와 약수.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+for(let i = 0 ; i < input.length-1 ; i++){
+    const [A, B] = input[i].split(" ").map(Number);
+
+    if(B % A === 0) {console.log("factor");}
+    else if(A % B === 0) {console.log("multiple");}
+    else{console.log("neither");}
+}
