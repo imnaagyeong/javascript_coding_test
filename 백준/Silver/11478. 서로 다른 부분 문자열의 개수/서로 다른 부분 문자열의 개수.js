74d8e0a3c67d8a99c20034ba3c1dd653d95cdd4a
Na-gang99 <155069538+Na-gang99@users.noreply.github.com>
2024-02-22 22:48:27 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 732 ms, Memory: 68848 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.js
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim();
+
 const str = new Set();
 for(let i = 0 ; i < input.length ; i++){
-    // i는 부분 문자열의 길이
     for(let j = i; j < input.length ; j++){
-        if(str.has(input.slice(i, j+1)) === false){
             str.add(input.slice(i, j+1))
-        }
+
     }
 }
 console.log(str.size);
