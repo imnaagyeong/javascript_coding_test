0a9ed6e97405895be77887feb418b1f79330017a
Na-gang99 <155069538+Na-gang99@users.noreply.github.com>
2024-03-04 13:44:44 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,11 @@
+function solution(a, d, included) {
+    let result = 0;
+    for(let i = 0 ; i < included.length ; i++){
+        if(included[i] === true){
+            result += a + d * i
+        }
+    }
+    
+    return result;
+}
+
