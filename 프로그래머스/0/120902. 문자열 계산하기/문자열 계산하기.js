d159424528242e0d9ca47b144a8616cbfcd06f7f
Na-gang99 <155069538+Na-gang99@users.noreply.github.com>
2024-03-28 10:11:37 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    var arr = my_string.split(" ");
+    var result = +arr[0];
+    for(let i = 1 ; i < arr.length ; i+= 2){
+        if(arr[i] === "+"){
+            result += Number(arr[i+1]);
+        }
+        else{
+            result -= Number(arr[i+1]);
+        }
+    }
+    return result;
+}
